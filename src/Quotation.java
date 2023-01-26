08bb75ae4a34fb1dd3154afb686f4c80c92f3e1d
mariiatsykura <117911005+mariiatsykura@users.noreply.github.com>
2023-01-26 17:49:59 +0200

    Add files via upload

---
+++ src/Quotation.java
@@ -0,0 +1,11 @@
+
+public class Quotation {
+
+    String quote = "“Do what you can, with what you have, where you are.”";
+    String author = " – Theodore Roosevelt";
+
+    public void display() {
+        System.out.println(quote);
+        System.out.println(author);
+    }
+}
